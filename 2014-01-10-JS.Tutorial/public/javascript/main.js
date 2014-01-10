80f42e128e46bd80c23aca5595e225c5078ef357
Maxwell Vance <maxistheman@comcast.net>
2014-01-10 15:21:24 -0600

    more JS stuff

--- 2014-01-10-JS.Tutorial/public/javascript/main.js
+++ 2014-01-10-JS.Tutorial/public/javascript/main.js
@@ -1,2 +1,33 @@
 console.log("Hello world");
+//this is a single line comment
+
+/******************************
+ * very cool
+ * multi
+ * line comment
+ * ****************************/
+console.log("Max Vance");
+/*
+  var a = 1;
+  var b = 2;
+  var c = a + b;
+  var d = c + b;
+  debugger;
+  var e = a + d;
+  console.log(e);
+*/
+console.log("The area of the room is " + (8 * 12) + " square feet.");
+console.log("The volume of the cylinder is " + (Math.PI * Math.pow(5, 2) * 9) + " square inches.");
+
+var room_a = 3 * 5;
+var room_b = 7 * 9;
+var room_c = 6 * 2;
+var per_house = room_a + room_b + room_c;
+var total = Math.floor(29572 / per_house);
+console.log("You can paint " + total + " houses.");
+
+/*
+andromeda_distance = 2540000;
+m_per_ly = 9.4607 * 10E15;
+*/
 
